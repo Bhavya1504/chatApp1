b33a97473273722f248b5918344875214aafa9c2
Bhavya1504 <124488278+Bhavya1504@users.noreply.github.com>
2023-08-18 12:44:19 +0530

    Create signup.js

---
+++ public/signup.js
@@ -0,0 +1,26 @@
+const url='http://localhost:3000'
+const save=async ()=>{
+    // e.preventDefault();
+    const name = document.getElementById('name');
+    const phoneNumber = document.getElementById('phoneNumber');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    let temp=[];
+    // temp=localStorage.getItem('chat');
+    // temp.push(`${name}`);
+
+    const obj={
+        name:name.value,
+        phone:phoneNumber.value,
+        email:email.value,
+        password:password.value
+    }
+    try {
+        const result=await axios.post(`${url}/signup`,obj);
+        console.log(result);
+        alert(result?.data?.message)
+        location=`${url}/login.html`
+    } catch (error) {
+        console.log(error);
+    }
+}
