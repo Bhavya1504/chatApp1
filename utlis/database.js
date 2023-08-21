cb9c41d2c8f28d813d5ba52621277f1ccb55fa39
Bhavya1504 <124488278+Bhavya1504@users.noreply.github.com>
2023-08-21 20:59:08 +0530

    Update database.js

--- utlis/database.js
+++ utlis/database.js
@@ -1,1 +1,10 @@
+const { Sequelize } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE_NAME, process.env.MYSQL_USER_NAME, process.env.MYSQL_USER_PASSWORD, {
+  host: process.env.HOST,
+  dialect:  'mysql' /* one of| 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+module.exports=sequelize;
 
